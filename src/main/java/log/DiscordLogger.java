2a1bbe74a36134490e196ceccd7090f554054624
motoki317 <motoki317@gmail.com>
2019-12-07 22:08:30 +0900

    Segregate spam checker

--- src/main/java/log/DiscordLogger.java
+++ src/main/java/log/DiscordLogger.java
@@ -2,31 +2,21 @@ package log;
 
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import utils.BotUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 public class DiscordLogger implements Logger {
-    // 1 second
-    private static final long SPAM_PREVENTION = TimeUnit.SECONDS.toMillis(1);
-
-    private static final long CACHE_TIME = TimeUnit.MINUTES.toMillis(1);
-
     private final DateFormat logFormat;
 
-    // User ID to last message time
-    private final Map<Long, Long> messages;
+    private final DiscordSpamChecker spamChecker;
 
     public DiscordLogger(TimeZone logTimeZone) {
         this.logFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
         this.logFormat.setTimeZone(logTimeZone);
-        this.messages = new HashMap<>();
+        this.spamChecker = new DiscordSpamChecker();
     }
 
     /**
@@ -48,39 +38,10 @@ public class DiscordLogger implements Logger {
      * @param event Guild message received event.
      */
     public boolean logEvent(MessageReceivedEvent event) {
-        boolean isSpam = isSpamMessage(event);
+        boolean isSpam = this.spamChecker.isSpam(event);
         String logMsg = this.createCommandLog(event, isSpam);
         this.log(4, logMsg);
         return isSpam;
-    }
-
-    /**
-     * Determines if the received event is a spam from the same user.
-     * @param event Message received event.
-     * @return True if this is a spam.
-     */
-    private boolean isSpamMessage(MessageReceivedEvent event) {
-        long userId = event.getAuthor().getIdLong();
-        long time = BotUtils.getIdCreationTime(event.getMessageIdLong());
-        long lastMessageTime = this.messages.getOrDefault(userId, -1L);
-
-        if (lastMessageTime == -1L) {
-            this.messages.put(userId, time);
-            return false;
-        }
-
-        long diff = time - lastMessageTime;
-        if (diff < SPAM_PREVENTION) {
-            return true;
-        }
-        this.messages.put(userId, lastMessageTime);
-
-        this.removeOldMessageCache(time);
-        return false;
-    }
-
-    private void removeOldMessageCache(long currentTime) {
-        this.messages.entrySet().removeIf((e) -> CACHE_TIME < (currentTime - e.getValue()));
     }
 
     /**
---
+++ src/main/java/log/DiscordSpamChecker.java
@@ -0,0 +1,46 @@
+package log;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import utils.BotUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+class DiscordSpamChecker {
+    // 1 second
+    private static final long SPAM_PREVENTION = TimeUnit.SECONDS.toMillis(1);
+
+    private static final long CACHE_TIME = TimeUnit.MINUTES.toMillis(1);
+
+    // User ID to last message time
+    private final Map<Long, Long> messages;
+
+    DiscordSpamChecker() {
+        this.messages = new HashMap<>();
+    }
+
+    boolean isSpam(MessageReceivedEvent event) {
+        long userId = event.getAuthor().getIdLong();
+        long time = BotUtils.getIdCreationTime(event.getMessageIdLong());
+        long lastMessageTime = this.messages.getOrDefault(userId, -1L);
+
+        if (lastMessageTime == -1L) {
+            this.messages.put(userId, time);
+            return false;
+        }
+
+        long diff = time - lastMessageTime;
+        if (diff < SPAM_PREVENTION) {
+            return true;
+        }
+        this.messages.put(userId, lastMessageTime);
+
+        this.removeOldMessageCache(time);
+        return false;
+    }
+
+    private void removeOldMessageCache(long currentTime) {
+        this.messages.entrySet().removeIf((e) -> CACHE_TIME < (currentTime - e.getValue()));
+    }
+}
