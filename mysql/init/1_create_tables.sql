af0e2912582f0740ada675d203c341b9bedebfde
motoki317 <motoki317@gmail.com>
2019-12-12 19:23:06 +0900

    fix table definition `track_channel` to support multiple guild/player tracks

--- mysql/init/1_create_tables.sql
+++ mysql/init/1_create_tables.sql
@@ -8,8 +8,10 @@ CREATE TABLE IF NOT EXISTS `track_channel` (
     `channel_id` BIGINT NOT NULL,
     `guild_name` VARCHAR(30),
     `player_name` VARCHAR(16),
-    PRIMARY KEY (`type`, `guild_id`, `channel_id`)
-);
+    `guild_name_v` VARCHAR(30) AS (IF(`guild_name` IS NULL, '', `guild_name`)) VIRTUAL,
+    `player_name_v` VARCHAR(16) AS (IF(`player_name` IS NULL, '', `player_name`)) VIRTUAL,
+    UNIQUE KEY (`type`, `guild_id`, `channel_id`, `guild_name_v`, `player_name_v`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `world` (
     `name` VARCHAR(30) NOT NULL,
@@ -17,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `world` (
     `created_at` TIMESTAMP DEFAULT NOW(),
     `updated_at` TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
     PRIMARY KEY (`name`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 # -- Test --
 DROP DATABASE IF EXISTS `moto-bot_test`;
@@ -26,13 +28,15 @@ CREATE DATABASE `moto-bot_test`;
 USE `moto-bot_test`;
 
 CREATE TABLE IF NOT EXISTS `track_channel` (
-   `type` VARCHAR(30) NOT NULL,
-   `guild_id` BIGINT NOT NULL,
-   `channel_id` BIGINT NOT NULL,
-   `guild_name` VARCHAR(30),
-   `player_name` VARCHAR(16),
-   PRIMARY KEY (`type`, `guild_id`, `channel_id`)
-);
+    `type` VARCHAR(30) NOT NULL,
+    `guild_id` BIGINT NOT NULL,
+    `channel_id` BIGINT NOT NULL,
+    `guild_name` VARCHAR(30),
+    `player_name` VARCHAR(16),
+    `guild_name_v` VARCHAR(30) AS (IF(`guild_name` IS NULL, '', `guild_name`)) VIRTUAL,
+    `player_name_v` VARCHAR(16) AS (IF(`player_name` IS NULL, '', `player_name`)) VIRTUAL,
+    UNIQUE KEY (`type`, `guild_id`, `channel_id`, `guild_name_v`, `player_name_v`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `world` (
     `name` VARCHAR(30) NOT NULL,
@@ -40,4 +44,4 @@ CREATE TABLE IF NOT EXISTS `world` (
     `created_at` TIMESTAMP DEFAULT NOW(),
     `updated_at` TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
     PRIMARY KEY (`name`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
