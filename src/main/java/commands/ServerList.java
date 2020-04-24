a2f12522bdb4d84c01f3aa0159b68130135cfa1d
motoki317 <motoki317@gmail.com>
2020-04-24 12:40:10 +0900

    refactor up display and max per page to 50

--- src/main/java/commands/ServerList.java
+++ src/main/java/commands/ServerList.java
@@ -12,6 +12,7 @@ import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import update.multipage.MultipageHandler;
 import update.reaction.ReactionManager;
 import utils.FormatUtils;
@@ -23,9 +24,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ServerList extends GenericCommand {
     private static final int WORLDS_PER_PAGE_DEFAULT = 20;
+    private static final int WORLDS_PER_PAGE_MAX = 50;
 
     private final WorldRepository repo;
     private final ReactionManager reactionManager;
@@ -77,54 +80,64 @@ public class ServerList extends GenericCommand {
         ).build();
     }
 
+    /**
+     * Retrieves world list.
+     * @param getAll If {@code true}, retrieves all worlds including lobby etc., but excluding WAR worlds.
+     * @return World list.
+     */
+    @Nullable
+    private List<World> getWorlds(boolean getAll) {
+        if (getAll) {
+            List<World> allWorlds = repo.findAll();
+            if (allWorlds == null) {
+                return null;
+            }
+            return allWorlds.stream().filter(w -> !w.getName().startsWith("WAR")).collect(Collectors.toList());
+        }
+        return repo.findAllMainWorlds();
+    }
+
     @Override
     public void process(@NotNull MessageReceivedEvent event, @NotNull String[] args) {
-        boolean all = false;
+        boolean getAll = false;
         int worldsPerPage = WORLDS_PER_PAGE_DEFAULT;
         if (args.length > 1) {
             for (String arg : args) {
-                if ("all".equals(arg.toLowerCase())) {
-                    all = true;
+                if ("getAll".equals(arg.toLowerCase())) {
+                    getAll = true;
                 } else if (arg.matches("\\d+")) {
                     worldsPerPage = Integer.parseInt(arg);
                 }
             }
         }
 
-        if (worldsPerPage < 1 || WORLDS_PER_PAGE_DEFAULT * 2 < worldsPerPage) {
+        if (worldsPerPage < 1 || WORLDS_PER_PAGE_MAX < worldsPerPage) {
             respond(event, "Invalid number specified for worlds per page argument.\n" +
-                    "Please specify a number between 1 and " + (WORLDS_PER_PAGE_DEFAULT * 2) + ".");
+                    "Please specify a number between 1 and " + WORLDS_PER_PAGE_MAX + ".");
             return;
         }
 
-        List<World> worlds;
-        if (all) {
-            List<World> allWorlds = repo.findAll();
-            if (allWorlds == null) {
-                respondError(event, "Something went wrong while retrieving data.");
-                return;
-            }
-            worlds = allWorlds.stream().filter(w -> !w.getName().startsWith("WAR")).collect(Collectors.toList());
-        } else {
-            worlds = repo.findAllMainWorlds();
-            if (worlds == null) {
-                respondError(event, "Something went wrong while retrieving data.");
-                return;
-            }
+        List<World> worlds = getWorlds(getAll);
+        if (worlds == null) {
+            respondError(event, "Something went wrong while retrieving data...");
+            return;
         }
-
         if (worlds.isEmpty()) {
-            respond(event, "There doesn't seem to be any " + (all ? "" : "main ") + "worlds online...");
+            respond(event, "There doesn't seem to be any " + (getAll ? "" : "main ") + "worlds online...");
             return;
         }
 
+        sendMessage(event, worldsPerPage, worlds);
+    }
+
+    // Send actual message
+    private void sendMessage(@NotNull MessageReceivedEvent event, int worldsPerPage, List<World> worlds) {
         CustomTimeZone timeZone = this.timeZoneRepository.getTimeZone(event);
         DateFormat dateFormat = this.dateFormatRepository.getDateFormat(event).getDateFormat().getMinuteFormat();
         dateFormat.setTimeZone(timeZone.getTimeZoneInstance());
 
-        int finalWorldsPerPage = worldsPerPage;
         Function<Integer, Message> pages = page ->
-                new MessageBuilder(format(worlds, page, finalWorldsPerPage, timeZone, dateFormat)).build();
+                new MessageBuilder(format(worlds, page, worldsPerPage, timeZone, dateFormat)).build();
         int maxPage = maxPage(worlds.size(), worldsPerPage);
 
         if (maxPage == 0) {
@@ -142,27 +155,22 @@ public class ServerList extends GenericCommand {
         return (worldsSize - 1) / worldsPerPage;
     }
 
+    private static class WorldDisplay {
+        private String num;
+        private String name;
+        private String players;
+        private String uptime;
+
+        private WorldDisplay(int num, String name, int players, String uptime) {
+            this.num = String.valueOf(num);
+            this.name = name;
+            this.players = String.valueOf(players);
+            this.uptime = uptime;
+        }
+    }
+
     @NotNull
     private static String format(@NotNull List<World> worlds, int page, int worldsPerPage, CustomTimeZone timeZone, DateFormat dateFormat) {
-        List<String> ret = new ArrayList<>();
-        ret.add("```ml");
-        ret.add("---- Server List ----");
-        ret.add("");
-
-        class WorldDisplay {
-            private int num;
-            private String name;
-            private int players;
-            private String uptime;
-
-            private WorldDisplay(int num, String name, int players, String uptime) {
-                this.num = num;
-                this.name = name;
-                this.players = players;
-                this.uptime = uptime;
-            }
-        }
-
         worlds.sort((w1, w2) -> (int) (w2.getCreatedAt().getTime() - w1.getCreatedAt().getTime()));
 
         long currentTimeMillis = System.currentTimeMillis();
@@ -179,45 +187,64 @@ public class ServerList extends GenericCommand {
             displays.add(new WorldDisplay(num, w.getName(), w.getPlayers(), uptime));
         }
 
-        int numLongest = 1;
-        int serverNameLongest = 6;
-        int playersLongest = 7;
-        int uptimeLongest = 6;
-        for (WorldDisplay display : displays) {
-            numLongest = Math.max(numLongest, String.valueOf(display.num).length());
-            serverNameLongest = Math.max(serverNameLongest, display.name.length());
-            playersLongest = Math.max(playersLongest, String.valueOf(display.players).length());
-            uptimeLongest = Math.max(uptimeLongest, display.uptime.length());
-        }
-
-        ret.add(nSpaces(numLongest + 2) + "Server" + nSpaces(serverNameLongest - 6) +
-                " | " + "Players" + nSpaces(playersLongest - 7) +
-                " | " + "Uptime");
-        ret.add(nHyphens(numLongest + 2 + serverNameLongest) +
-                "-+-" + nHyphens(playersLongest) +
-                "-+-" + nHyphens(uptimeLongest + 1));
-
-        for (WorldDisplay display : displays) {
-            String toAdd = String.format("%-" + (numLongest + 1) + "s", display.num + ".");
-            toAdd += " ";
-            toAdd += String.format("%-" + serverNameLongest + "s", display.name);
-            toAdd += " | ";
-            toAdd += String.format("%-" + playersLongest + "s", display.players);
-            toAdd += " | ";
-            toAdd += String.format("%" + uptimeLongest + "s", display.uptime);
-
-            ret.add(toAdd);
-        }
-
-        ret.add("");
-
         int maxPage = maxPage(worlds.size(), worldsPerPage);
-        ret.add(String.format("< page %s / %s >", page + 1, maxPage + 1));
+        String pageView = String.format("< page %s / %s >", page + 1, maxPage + 1);
+
+        return formatDisplays(
+                displays,
+                worlds.stream().mapToLong(d -> d.getUpdatedAt().getTime()).max().orElse(System.currentTimeMillis()),
+                dateFormat,
+                timeZone,
+                pageView
+        );
+    }
+
+    private static String formatDisplays(List<WorldDisplay> displays,
+                                         long lastUpdate, DateFormat dateFormat, CustomTimeZone customTimeZone,
+                                         String pageView) {
+        int numJustify = displays.stream().mapToInt(d -> d.num.length()).max().orElse(1);
+        int serverNameJustify = IntStream.concat(
+                IntStream.of("Server".length()),
+                displays.stream().mapToInt(d -> d.name.length())
+        ).max().orElse("Server".length());
+        int playersJustify = IntStream.concat(
+                IntStream.of("Players".length()),
+                displays.stream().mapToInt(d -> d.players.length())
+        ).max().orElse("Players".length());
+        int uptimeJustify = IntStream.concat(
+                IntStream.of("Uptime".length()),
+                displays.stream().mapToInt(d -> d.uptime.length())
+        ).max().orElse("Uptime".length());
+
+        List<String> ret = new ArrayList<>();
+        ret.add("```ml");
+        ret.add("---- Server List ----");
+        ret.add("");
+
+        ret.add(nSpaces(numJustify + 2) + "Server" + nSpaces(serverNameJustify - 6) +
+                " | " + "Players" + nSpaces(playersJustify - 7) +
+                " | " + "Uptime");
+        ret.add(nHyphens(numJustify + 2 + serverNameJustify) +
+                "-+-" + nHyphens(playersJustify) +
+                "-+-" + nHyphens(uptimeJustify + 1));
+
+        for (WorldDisplay d : displays) {
+            ret.add(String.format("%s.%s %s%s | %s%s | %s%s",
+                    d.num, nSpaces(numJustify - d.num.length()),
+                    d.name, nSpaces(serverNameJustify - d.name.length()),
+                    d.players, nSpaces(playersJustify - d.players.length()),
+                    nSpaces(uptimeJustify - d.uptime.length()), d.uptime
+            ));
+        }
 
         ret.add("");
 
-        Date lastUpdate = new Date(worlds.get(0).getUpdatedAt().getTime());
-        ret.add(String.format("last update: %s %s", dateFormat.format(lastUpdate), timeZone.getTimezone()));
+        ret.add(pageView);
+
+        ret.add("");
+
+        ret.add(String.format("last update: %s (%s)",
+                dateFormat.format(new Date(lastUpdate)), customTimeZone.getFormattedTime()));
 
         ret.add("```");
         return String.join("\n", ret);
