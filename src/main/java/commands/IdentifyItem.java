d4a5840e516f8e798e36bf60f22cef50792f8293
motoki317 <motoki317@gmail.com>
2022-02-10 19:27:08 +0900

    🚧 add item / id cmds to new interface

--- src/main/java/commands/IdentifyItem.java
+++ src/main/java/commands/IdentifyItem.java
@@ -5,14 +5,25 @@ import api.wynn.structs.Item;
 import api.wynn.structs.ItemDB;
 import app.Bot;
 import commands.base.GenericCommand;
+import commands.event.CommandEvent;
+import commands.event.InteractionHookAdapter;
+import commands.event.SentMessageAdapter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Emoji;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.exceptions.PermissionException;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
 import org.jetbrains.annotations.NotNull;
+import update.button.ButtonClickHandler;
+import update.button.ButtonClickManager;
 import update.reaction.ReactionManager;
 import update.reaction.ReactionResponse;
 import utils.ArgumentParser;
@@ -28,17 +39,33 @@ public class IdentifyItem extends GenericCommand {
     private final WynnApi wynnApi;
     private final String imageURLBase;
     private final ReactionManager reactionManager;
+    private final ButtonClickManager buttonClickManager;
 
     public IdentifyItem(Bot bot) {
         this.wynnApi = new WynnApi(bot.getLogger());
         this.imageURLBase = bot.getProperties().githubImagesUrl;
         this.reactionManager = bot.getReactionManager();
+        this.buttonClickManager = bot.getButtonClickManager();
     }
 
     @NotNull
     @Override
     protected String[][] names() {
         return new String[][]{{"id", "identify"}};
+    }
+
+    @Override
+    public @NotNull String[] slashName() {
+        return new String[]{"id"};
+    }
+
+    @Override
+    public @NotNull OptionData[] slashOptions() {
+        return new OptionData[]{
+                new OptionData(OptionType.STRING, "name", "Name of item to identify", true),
+                new OptionData(OptionType.STRING, "re-identify", "Show re-ID buttons")
+                        .addChoice("-re", "-re")
+        };
     }
 
     @Override
@@ -70,15 +97,15 @@ public class IdentifyItem extends GenericCommand {
     }
 
     @Override
-    public void process(@NotNull MessageReceivedEvent event, @NotNull String[] args) {
+    public void process(@NotNull CommandEvent event, @NotNull String[] args) {
         if (args.length < 2) {
-            respond(event, "Please input the item name you want to simulate identifying.");
+            event.reply("Please input the item name you want to simulate identifying.");
             return;
         }
 
         ItemDB db = this.wynnApi.mustGetItemDB(false);
         if (db == null) {
-            respondError(event, "Something went wrong while requesting Wynncraft API.");
+            event.replyError("Something went wrong while requesting Wynncraft API.");
             return;
         }
 
@@ -88,10 +115,10 @@ public class IdentifyItem extends GenericCommand {
         List<Item> matched = searchItem(input, db.getItems());
 
         if (matched.size() == 0) {
-            respond(event, String.format("No items matched with input `%s`.", input));
+            event.reply(String.format("No items matched with input `%s`.", input));
             return;
         } else if (matched.size() > 1) {
-            respond(event, String.format("Multiple items (%s items) matched with input `%s`.\nMatched items: %s",
+            event.reply(String.format("Multiple items (%s items) matched with input `%s`.\nMatched items: %s",
                     matched.size(), input,
                     matched.stream().limit(50).map(i -> "`" + i.getName() + "`")
                             .collect(Collectors.joining(", "))));
@@ -102,14 +129,20 @@ public class IdentifyItem extends GenericCommand {
 
         boolean reIdentify = parser.getArgumentMap().containsKey("re");
         if (!reIdentify) {
-            respond(event, formatItemInfo(item, this.imageURLBase, 1));
+            event.reply(formatItemInfo(item, this.imageURLBase, 1));
             return;
         }
 
-        respond(event, formatItemInfo(item, this.imageURLBase, 1), message -> {
-            IDReactionHandler handler = new IDReactionHandler(message, event.getAuthor().getIdLong(),
-                    seq -> formatItemInfo(item, this.imageURLBase, seq));
-            this.reactionManager.addEventListener(handler);
+        event.reply(formatItemInfo(item, this.imageURLBase, 1), message -> {
+            // probably not the best code
+            if (message instanceof SentMessageAdapter sm) {
+                IDReactionHandler handler = new IDReactionHandler(sm.m(), event.getAuthor().getIdLong(),
+                        seq -> formatItemInfo(item, this.imageURLBase, seq));
+                this.reactionManager.addEventListener(handler);
+            } else if (message instanceof InteractionHookAdapter hook) {
+                IDButtonHandler handler = new IDButtonHandler(hook.hook(), seq -> formatItemInfo(item, this.imageURLBase, seq));
+                this.buttonClickManager.addEventListener(handler);
+            }
         });
     }
 
@@ -157,6 +190,7 @@ public class IdentifyItem extends GenericCommand {
 
         /**
          * Adds paging reactions to a message.
+         *
          * @param message Message to add reactions to.
          */
         private static void addPagingReactions(Message message) {
@@ -167,6 +201,7 @@ public class IdentifyItem extends GenericCommand {
 
         /**
          * Deletes paging reactions from the sent message.
+         *
          * @param self Bot user.
          */
         private void deletePagingReactions(User self) {
@@ -176,11 +211,66 @@ public class IdentifyItem extends GenericCommand {
         }
     }
 
+    private static class IDButtonHandler extends ButtonClickHandler {
+        private static final String BUTTON_ID_REFRESH = "refresh";
+        private static final String BUTTON_ID_CANCEL = "cancel";
+
+        private final InteractionHook hook;
+        private final Function<Integer, Message> messageSupplier;
+        private int seq;
+
+        public IDButtonHandler(InteractionHook hook, Function<Integer, Message> messageSupplier) {
+            super(hook.getInteraction().getIdLong(), (event) -> false, () -> {
+            });
+
+            this.hook = hook;
+            this.messageSupplier = messageSupplier;
+            this.seq = 1;
+
+            this.hook.editOriginalComponents(
+                    ActionRow.of(
+                            Button.secondary(BUTTON_ID_REFRESH, Emoji.fromUnicode("\u2705")),
+                            Button.danger(BUTTON_ID_CANCEL, Emoji.fromUnicode("\u274C"))
+                    )
+            ).queue();
+        }
+
+        @Override
+        public boolean handle(ButtonClickEvent event) {
+            super.handle(event);
+
+            if (event.getButton() == null || event.getButton().getId() == null) return false;
+
+            switch (event.getButton().getId()) {
+                case BUTTON_ID_REFRESH:
+                    break;
+                case BUTTON_ID_CANCEL:
+                    return true;
+            }
+
+            this.seq++;
+            this.hook.editOriginal(
+                    messageSupplier.apply(this.seq)
+            ).queue();
+
+            return false;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+
+            // Delete buttons
+            this.hook.editOriginalComponents().queue();
+        }
+    }
+
     /**
      * Formats item info and returns formatted message.
-     * @param item Item
+     *
+     * @param item         Item
      * @param imageURLBase Image URl
-     * @param seq How many times this item has been identified. Used for display.
+     * @param seq          How many times this item has been identified. Used for display.
      * @return Formatted message.
      */
     private static Message formatItemInfo(Item item, String imageURLBase, int seq) {
@@ -202,7 +292,7 @@ public class IdentifyItem extends GenericCommand {
 
     private static String getIDs(Item item) {
         List<Identification> availableIDs = Arrays.stream(identifications)
-                .filter(i -> i.status.apply(item) != 0).collect(Collectors.toList());
+                .filter(i -> i.status.apply(item) != 0).toList();
 
         List<String> ret = new ArrayList<>();
         if (availableIDs.isEmpty()) {
